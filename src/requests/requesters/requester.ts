bcbd5ebfaebd620856c30091bdcfbfb65d493b28
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-07-22 22:53:25 +1000

    Add game creation test

--- src/requests/requesters/requester.ts
+++ src/requests/requesters/requester.ts
@@ -7,5 +7,11 @@ import { RequestDto } from "../dto";
 export abstract class Requester {
   constructor(public readonly dto: RequestDto) {}
 
+  /**
+   * Gets or creates the user who made the request.
+   *
+   * @abstract
+   * @returns {Promise<Either<Failure<UserFailureTypes>, User>>}
+   */
   public abstract async getOrCreateUser(): Promise<Either<Failure<UserFailureTypes>, User>>;
 }
