d79bbbbb750f3888b8ed6f346d5bc3b650a6424e
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-08-01 20:22:48 +1000

    Added a mocha test env variable in order to only start the Discord bot when not running tests

--- src/index.ts
+++ src/index.ts
@@ -32,25 +32,26 @@ import { DiscordBot } from "./discord/discord-bot";
 
 Message.enableSentenceCaseOutput();
 
-(() => {
-  setTimeout(async () => {
-    try {
-      // create typeorm connection
-      // await ConnectionManager.getInstance();
+if (process.env.NODE_ENV !== "test")
+  (() => {
+    setTimeout(async () => {
+      try {
+        // create typeorm connection
+        // await ConnectionManager.getInstance();
 
-      // const gameController = iocContainer.get(entities.GameController);
-      // const response = await gameController.create({
-      //   gameDto: { teamLives: 2, countFailedScores: true },
-      //   requestDto: { type: "discord", authorId: "waffle", originChannel: "waffle's amazing channel" }
-      // });
-      // var a = response;
+        // const gameController = iocContainer.get(entities.GameController);
+        // const response = await gameController.create({
+        //   gameDto: { teamLives: 2, countFailedScores: true },
+        //   requestDto: { type: "discord", authorId: "waffle", originChannel: "waffle's amazing channel" }
+        // });
+        // var a = response;
 
-      const discordBot = new DiscordBot();
-      await discordBot.start(process.env.DISCORD_BOT_TOKEN);
-    } catch (e) {
-      console.error(e);
-    }
-  }, (2 ^ 32) - 1);
-})();
+        const discordBot = new DiscordBot();
+        await discordBot.start(process.env.DISCORD_BOT_TOKEN);
+      } catch (e) {
+        console.error(e);
+      }
+    }, (2 ^ 32) - 1);
+  })();
 
 export {};
