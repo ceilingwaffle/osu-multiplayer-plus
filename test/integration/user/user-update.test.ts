b65031c786d031a3dae178e997d81ce10071f056
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-09-06 11:36:20 +1000

    Comment-out unfinished tests

--- test/integration/user/user-update.test.ts
+++ test/integration/user/user-update.test.ts
@@ -1,3 +1,3 @@
-describe("When updating a user", function() {
-  it("updates the country code of a user if the user has moved country");
-});
+// describe("When updating a user", function() {
+//   it("updates the country code of a user if the user has moved country");
+// });
