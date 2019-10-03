52764d91d274ca93108e69d87e154181809c4187
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-10-03 20:25:41 +1000

    Add random int generator method to Helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -86,4 +86,19 @@ export class Helpers {
     if (typeof s !== "string") return "";
     return s.charAt(0).toUpperCase() + s.slice(1);
   }
+
+  /**
+   * Returns a random integer between min (inclusive) and max (inclusive).
+   * The value is no lower than min (or the next integer greater than min
+   * if min isn't an integer) and no greater than max (or the next integer
+   * lower than max if max isn't an integer).
+   * Using Math.round() will give you a non-uniform distribution!
+   *
+   * Source: https://stackoverflow.com/a/1527820/11622198
+   */
+  static getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
