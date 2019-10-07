41b85271f6e2d66b509130a937df4a424bff9967
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-10-08 05:44:58 +1100

    Make Dispatcher generic

--- src/utils/dispatcher.ts
+++ src/utils/dispatcher.ts
@@ -1,28 +1,30 @@
-export interface Event<T extends EventName, D> extends EventData<D> {
-  type: T;
+export interface Event<EventName, D> extends EventData<D> {
+  type: EventName;
 }
 interface EventData<D> {
   payload: D;
 }
-export type EventType = LeaderboardReadyEvent | FetchedNewOsuApiMultiplayerResults;
-type Handler = (event: EventType) => boolean;
-type EventHandlers = Set<Handler>;
-interface IDispatcher {
-  handlers: Map<EventName, EventHandlers>;
-  subscribe(eventName: EventName, handler: Handler): void;
-  dispatch(eventName: EventData<any>): void;
+type Handler<EventName, EventData> = (eventType: EventName, eventData: EventData) => boolean;
+type EventHandlers<EventName, EventData> = Set<Handler<EventName, EventData>>;
+
+interface IDispatcher<EventName, EventData> {
+  handlers: Map<EventName, EventHandlers<EventName, EventData>>;
+  subscribe(eventName: EventName, handler: Handler<EventName, EventData>): void;
+  dispatch(event: Event<EventName, EventData>): void;
 }
 
-export class Dispatcher implements IDispatcher {
-  handlers: Map<EventName, EventHandlers> = new Map<EventName, EventHandlers>();
+export class Dispatcher<EventName, EventData> implements IDispatcher<EventName, EventData> {
+  handlers: Map<EventName, EventHandlers<EventName, EventData>> = new Map<EventName, EventHandlers<EventName, EventData>>();
 
-  subscribe(eventName: EventName, handler: Handler): void {
-    this.handlers.get(eventName) ? this.handlers.get(eventName).add(handler) : this.handlers.set(eventName, new Set<Handler>([handler]));
+  subscribe(eventName: EventName, handler: Handler<EventName, EventData>): void {
+    this.handlers.get(eventName)
+      ? this.handlers.get(eventName).add(handler)
+      : this.handlers.set(eventName, new Set<Handler<EventName, EventData>>([handler]));
   }
 
-  dispatch(event: EventType): void {
+  dispatch(event: Event<EventName, EventData>): void {
     for (const handlerFn of this.handlers.get(event.type)) {
-      const handledResult = handlerFn(event);
+      const handledResult = handlerFn(event.type, event.payload);
       if (!handledResult) {
         break;
       }
@@ -30,6 +32,8 @@ export class Dispatcher implements IDispatcher {
   }
 }
 // -----------------------------------
+
+export type EventType = LeaderboardReadyEvent | FetchedNewOsuApiMultiplayerResults;
 
 interface LeaderboardData {
   leaderboard: string;
@@ -43,13 +47,13 @@ type EventName = "LEADERBOARD_READY" | "FETCHED_NEW_OSU_API_MULTIPLAYER_RESULTS"
 interface LeaderboardReadyEvent extends Event<"LEADERBOARD_READY", LeaderboardData> {}
 interface FetchedNewOsuApiMultiplayerResults extends Event<"FETCHED_NEW_OSU_API_MULTIPLAYER_RESULTS", ApiMultiplayerData> {}
 
-function leaderboardHandler1(event: EventType): boolean {
-  console.log(`Calling leaderboardHandler1`, event);
+function leaderboardHandler1(event: EventName, payload: EventData<LeaderboardData>): boolean {
+  console.log(`Calling leaderboardHandler1`, { event, payload });
   return true;
 }
 
-function leaderboardHandler2(event: EventType): boolean {
-  console.log(`Calling leaderboardHandler2`, event);
+function leaderboardHandler2(event: EventType, payload: EventData<LeaderboardData>): boolean {
+  console.log(`Calling leaderboardHandler2`, { event, payload });
   return true;
 }
 
