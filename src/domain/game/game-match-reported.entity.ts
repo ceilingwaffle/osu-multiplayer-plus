8678f2610a06ff88217908633a6b7846b8611c66
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-10-17 23:37:28 +1100

    Added context props to GameMatchReportedEntity

--- src/domain/game/game-match-reported.entity.ts
+++ src/domain/game/game-match-reported.entity.ts
@@ -1,8 +1,16 @@
-import { Entity, ManyToOne } from "typeorm";
+import { Entity, ManyToOne, Column, PrimaryGeneratedColumn } from "typeorm";
 import { CreationTimestampedEntity } from "../shared/creation-timestamped-entity";
 import { Game } from "./game.entity";
 import { Match } from "../match/match.entity";
 import { Realm } from "../realm/realm.entity";
+import { MessageType } from "../../multiplayer/lobby-beatmap-status-message";
+import { GameEventType } from "../../multiplayer/game-events/game-event-types";
+
+export enum ReportedType {
+  "message" = "message",
+  "event" = "event"
+}
+
 /**
  * Entity used to denote which matches have been"reported" for a game. "Reported" meaning that a leaderboard and lobby-messages
  * like "Lobby 1 completed beatmap2#1" have been delivered to some realm. This is needed so that we don't send duplicate
@@ -14,12 +22,36 @@ import { Realm } from "../realm/realm.entity";
  */
 @Entity("games_matches_reported")
 export class GameMatchReported extends CreationTimestampedEntity {
-  @ManyToOne(type => Game, game => game.gameMatchesReported, { primary: true })
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(type => Game, game => game.gameMatchesReported)
   game: Game;
 
-  @ManyToOne(type => Match, match => match.gameMatchesReported, { primary: true })
+  @ManyToOne(type => Match, match => match.gameMatchesReported)
   match: Match;
+
+  @Column({ type: "simple-enum", enum: ReportedType })
+  reportedType: ReportedType;
+
+  @Column("simple-json")
+  reportedContext: { type: "message" | "game_event"; subType: MessageType | GameEventType; sameBeatmapNumber: number; beatmapId: string };
 
   @ManyToOne(type => Realm, { nullable: true })
   reportedToRealms: Realm[];
 }
+
+export type ReportedContextType = "message" | "game_event";
+
+export type ReportedContext<T extends ReportedContextType> = {
+  type?: T;
+  subType: T extends "message" ? MessageType : T extends "game_event" ? GameEventType : never;
+  sameBeatmapNumber: number;
+  beatmapId: string;
+};
+
+// const foo: ReportedContext<"message"> = {
+//   sameBeatmapNumber: 1,
+//   beatmapId: "foo",
+//   subType: "all_lobbies_completed"
+// };
