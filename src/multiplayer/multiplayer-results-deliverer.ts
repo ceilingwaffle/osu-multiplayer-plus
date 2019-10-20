8d1732af21967707135a4dcef923a18c171be827
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-10-21 04:52:55 +1100

    Inject Dispatcher in MultiplayerResultsDeliverer from inversify IoC container

--- src/multiplayer/multiplayer-results-deliverer.ts
+++ src/multiplayer/multiplayer-results-deliverer.ts
@@ -1,11 +1,20 @@
+import { injectable, inject } from "inversify";
+import TYPES from "../types";
+import { IEventDispatcher } from "../utils/event-dispatcher";
 import { ReportableContext, ReportableContextType } from "../domain/game/game-match-reported.entity";
 
+@injectable()
 export class MultiplayerResultsDeliverer {
+  constructor(@inject(TYPES.IEventDispatcher) protected dispatcher: IEventDispatcher) {}
+
   async deliver(args: { reportables: ReportableContext<ReportableContextType>[] }): Promise<boolean> {
     // should fire an event such that a DiscordMessager and WebRelayer can deliver the reportable messages
     // (the messager classes should be responsible for creating the message string - this class only delivers the view-data)
     //
     // this might be time for the dispatcher
+
+    // this.dispatcher.dispatch(new event with payload)
+
     throw new Error("TODO: Implement method of MultiplayerResultsDeliverer.");
   }
 }
