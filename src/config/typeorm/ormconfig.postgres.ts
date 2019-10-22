6fd491771501aec484aaccc84e61c92684b7264e
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-10-22 18:25:32 +1100

    Log more in typeorm postgres logger

--- src/config/typeorm/ormconfig.postgres.ts
+++ src/config/typeorm/ormconfig.postgres.ts
@@ -13,7 +13,8 @@ const config: PostgresConnectionOptions = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DBNAME,
   synchronize: true, // this will keep database data but it will re-create the schema at every app launch. Same as: npm run typeorm schema:sync
-  logging: ["warn"],
+  logger: "file",
+  logging: ["query", "warn", "error"],
   entities: ["src/domain/**/*.entity.ts"],
   subscribers: ["src/domain/**/*.subscriber.ts"],
   migrations: ["src/database/migrations/**/*.ts"],
