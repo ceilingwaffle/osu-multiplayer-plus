1668e2f43a4f86e187c1091bd383b6d16121a43a
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-11-03 15:07:32 +1100

    Improve database seeder intiialization

--- cli/seed-db.ts
+++ cli/seed-db.ts
@@ -3,29 +3,34 @@ import { IDbClient } from "../src/database/db-client";
 import TYPES from "../src/types";
 import { Log } from "../src/utils/Log";
 import CreateUsersSeeder from "../src/database/seeds/create-users.seeder";
-
-const runSeeders = async function() {
-  try {
-    const dbClient = iocContainer.get<IDbClient>(TYPES.IDbClient);
-    const connection = await dbClient.connectIfNotConnected();
-
-    // TODO: Get user confirmation to drop database before seeding
-    // TODO: Drop database
-
-    const usersSeeder: CreateUsersSeeder = new CreateUsersSeeder();
-    await usersSeeder.run(connection);
-  } catch (error) {
-    throw error;
-  }
-};
+import { Seeder } from "../src/database/seeder";
 
 setTimeout(async () => {
   try {
-    Log.info("Seeding database...");
     await runSeeders();
-    Log.info("Seeding completed successfully.");
   } catch (e) {
     Log.error(e);
     throw e;
   }
 }, (2 ^ 32) - 1);
+
+const runSeeders = async function() {
+  try {
+    Log.info("Seeding database...");
+    const dbClient = iocContainer.get<IDbClient>(TYPES.IDbClient);
+    const connection = await dbClient.connectIfNotConnected();
+
+    // TODO: Get user confirmation to drop database before seeding
+
+    // initialize seeders
+    const seeders: Seeder[] = [new CreateUsersSeeder()];
+
+    // run seeders
+    for (const seeder of seeders) {
+      await seeder.run(connection);
+    }
+    Log.info("Seeding completed successfully.");
+  } catch (error) {
+    throw error;
+  }
+};
--- cli/seed-test-db.ts
+++
@@ -1,7 +0,0 @@
-import { TestHelpers } from "../test/test-helpers";
-
-console.log(`Executing ${__dirname}/${__filename}...`);
-
-(async function() {
-  await TestHelpers.seedTestDatabase();
-})();
