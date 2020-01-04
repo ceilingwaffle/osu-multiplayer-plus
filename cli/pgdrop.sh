b72cec2425920ceeade4c9101d5ca7c5a005b4fc
Ceiling Waffle <ceilingwaffle@gmail.com>
2020-01-04 17:12:13 +1100

    Update pgdrop script

--- cli/pgdrop.sh
+++ cli/pgdrop.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 # Drops and recreated the postgres database
 
-read -p "Are you sure? " -n 1 -r
+# must be root
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
+
+# destructive confirmation
+read -p "Are you sure? (y)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 fi
 
+# execute script from relative path instead of pwd
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+# read vars from .env
 read_var() {
     VAR=$(grep $1 $2 | xargs)
     IFS="=" read -ra VAR <<< "$VAR"
@@ -22,4 +34,3 @@ POSTGRES_USERNAME=$(read_var POSTGRES_USERNAME $DOT_ENV_PATH)
 sudo -u postgres -h $POSTGRES_HOSTNAME -p $POSTGRES_PORT psql -c "drop database $POSTGRES_DBNAME;"
 sudo -u postgres -h $POSTGRES_HOSTNAME -p $POSTGRES_PORT createdb $POSTGRES_DBNAME
 sudo -u postgres -h $POSTGRES_HOSTNAME -p $POSTGRES_PORT psql -c "grant all privileges on database $POSTGRES_DBNAME to $POSTGRES_USERNAME;"
-
